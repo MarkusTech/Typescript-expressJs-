f396c3bc8444a7aaddefd428b46eee23ecd90ccf
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-29 16:05:11 +0800

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,21 @@ import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import noteModel from "./models/note";
 import { isHttpError } from "http-errors";
+import morgan from "morgan";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+// dotenv config
 dotenv.config();
 
+// Rest Object
 const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded());
+
 app.get("/", async (req, res) => {
   const notes = await noteModel.find().exec();
   res.status(200).json({
@@ -15,6 +26,7 @@ app.get("/", async (req, res) => {
   });
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response) => {
   console.error(error);
   let errorMessage = "An unknown error occurred";
