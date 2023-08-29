ce147836568427a8ca671c73e7c14e53d1a3f28f
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-29 11:49:27 +0800

    error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import colors from "colors";
+
 const app = express();
 const port = 5000;
 
@@ -7,5 +9,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  colors;
+  console.log(`Server is running on http://localhost:${port}`.bgCyan);
 });
