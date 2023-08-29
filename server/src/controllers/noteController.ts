28274295801ffe19ce61e4ad8c3d0e3ec27bb093
Markus Cabaron <55816864+MarkusTech@users.noreply.github.com>
2023-08-29 16:53:10 +0800

    Update noteController.ts

--- server/src/controllers/noteController.ts
+++ server/src/controllers/noteController.ts
@@ -1,5 +1,7 @@
+import mongoose from "mongoose";
 import noteModel from "../models/note";
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
 
 const getNotes: RequestHandler = async (req, res, next) => {
   try {
@@ -14,4 +16,27 @@ const getNotes: RequestHandler = async (req, res, next) => {
   }
 };
 
-export { getNotes };
+const getNote: RequestHandler = async (req, res, next) => {
+  const { noteId } = req.params;
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    const note = await noteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
+
+    // if (!note.userId.equals(authenticatedUserId)) {
+    //     throw createHttpError(401, "You cannot access this note");
+    // }
+
+    res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getNotes, getNote };
